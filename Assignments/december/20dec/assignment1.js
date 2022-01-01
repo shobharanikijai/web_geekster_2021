ad7f28840aceb4d11835c70b7a14c8c9711f191a
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-01 21:58:14 +0530

    Create assignment1.js

---
+++ Assignments/december/20dec/assignment1.js
@@ -0,0 +1,8 @@
+const number = 70;
+
+// check if number is greater than 10
+if (number > 10) {
+    console.log("The number "+number+" is greater than 10");
+}else{
+    console.log("The number "+number+" is not greater than 10");
+}
