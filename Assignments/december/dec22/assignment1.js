424b3d2fe9f2636963fc287f4fb3f9c3b06a717b
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-01 23:04:40 +0530

    Create assignment1.js

---
+++ Assignments/december/dec22/assignment1.js
@@ -0,0 +1,12 @@
+//create an array using .push with prompt and also using parseInt(); 
+num = [4,2];
+console.log("Before: " +num);
+num.push(7,1);
+var a = parseInt(20);
+num.push(a)
+console.log("After: " +num);
+console.log("The highest number is " +Math.max(...num));
+
+
+//Take three numbers and find max. (Using arrays) -> Math.max(); 
+// console.log( "The highest number is " +Math.max(1, 3, 2));
