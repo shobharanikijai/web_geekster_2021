7f335e5bb6b641cbac7e59377925c5a4206ce6f1
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-01 23:05:53 +0530

    Update assignment1.js

--- Assignments/december/dec22/assignment1.js
+++ Assignments/december/dec22/assignment1.js
@@ -9,4 +9,4 @@ console.log("The highest number is " +Math.max(...num));
 
 
 //Take three numbers and find max. (Using arrays) -> Math.max(); 
-// console.log( "The highest number is " +Math.max(1, 3, 2));
+console.log( "The highest number is " +Math.max(1, 3, 2));
