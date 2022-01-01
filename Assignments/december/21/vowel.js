80aa97eb36f3584465eb8bcd61bc895d8577925d
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-01 22:24:18 +0530

    Create vowel.js

---
+++ Assignments/december/21/vowel.js
@@ -0,0 +1,25 @@
+//Check whether the given chracter is a vowel or not. (Also check for capital characters).
+var char = "O";
+
+if(char == "a" ) {
+ console.log(char+" is a vowel. It is a small letter");
+} else if(char == "e") {
+ console.log(char+"It is a vowel. It is a small letter");
+} else if(char == "i") {
+ console.log(char+"It is a vowel. It is a small letter");
+} else if(char == "o") {
+ console.log(char+"It is a vowel. It is a small letter");
+} else if(char == "u") {
+ console.log(char+"It is a vowel. It is a small letter");
+} else if(char == "A") {
+ console.log(char+"It is a vowel. It is a capital letter");
+} else if(char == "E") {
+ console.log(char+"It is a vowel. It is a capital letter");
+} else if(char == "I") {
+ console.log(char+"It is a vowel. It is a capital letter");
+} else if(char == "O") {
+ console.log(char+"It is a vowel. It is a capital letter");
+} else if(char == "U") {
+ console.log(char+"It is a vowel. It is a capital letter");
+} else
+    console.log("It is not a vowel!");
