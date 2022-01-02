76e9ee88f79bf6ee36d9e3d5a694531f90daa104
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-02 12:15:43 +0530

    Update prime.js

--- Assignments/december/dec23/prime.js
+++ Assignments/december/dec23/prime.js
@@ -1,6 +1,25 @@
-//Assignment3: -> Write a program to take an input from the user (num) and check whether it is a prime number or not.
-var num = 4;
-if (num%2 == 0)
-    console.log("It is a prime");
-else
-    console.log("It is not a prime");
+// Write a program to take an input from the user (num) and check whether it is a prime number or not.
+
+const number = parseInt(prompt("Enter a number: "));
+let isPrime = true;
+if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
+}
+else if (number > 1) {
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
+
+else {
+    console.log("The number is not a prime number.");
+}
