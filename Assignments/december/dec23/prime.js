8e6c07cb5fe4c8b213d226bec5cdee5bf943d4c7
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-02 11:56:41 +0530

    Create prime.js

---
+++ Assignments/december/dec23/prime.js
@@ -0,0 +1,6 @@
+//Assignment3: -> Write a program to take an input from the user (num) and check whether it is a prime number or not.
+var num = 4;
+if (num%2 == 0)
+    console.log("It is a prime");
+else
+    console.log("It is not a prime");
