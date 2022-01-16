1e53437646ae2b344e704dc1be5e50fb069c0bb9
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-16 20:49:08 +0530

    Add files via upload

---
+++ Assignments/january/jan11/script.js
@@ -0,0 +1,10 @@
+var n = parseInt(prompt("Enter a Number"));
+
+for ( var i=1 ; i<=n ; i++){
+
+    for(var j=1;j<i+1;j++){
+       document.write("* ");
+    }
+    document.write("<br>");
+    
+}
