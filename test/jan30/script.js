ee74e9fd3c05191ecb4a7f7b45faef5d24f6b29e
Hringam Baba <94857583+shobharanikijai@users.noreply.github.com>
2022-01-30 23:55:22 +0530

    Update script.js

--- test/jan30/script.js
+++ test/jan30/script.js
@@ -1,45 +1,14 @@
-// api url
-const api_url = "http://universities.hipolabs.com/search?country={country_name}";
-
-
-// Defining async function
-async function getapi(url) {
-	
-	// Storing response
-	const response = await fetch(url);
-	
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-		hideloader();
-	}
-	show(data);
-}
-// Calling that async function
-getapi(api_url);
-
-// Function to hide the loader
-function hideloader() {
-	document.getElementById('loading').style.display = 'none';
-}
-// Function to define innerHTML for HTML table
-function show(data) {
-	let tab =
-		`<tr>
-		<th>state-province</th>
-		<th>name</th>
-		<th>web_pages</th>
-		</tr>`;
-	
-	// Loop to access all rows
-	for (let r of data.list) {
-		tab += `<tr>
-	<td>${r.state-province} </td>
-	<td>${r.name}</td>
-	<td>${r.web_pages}</td>		
-</tr>`;
-	}
-	// Setting innerHTML as tab variable
-	document.getElementById("search").innerHTML = tab;
-}
+fetch('http://universities.hipolabs.com/search?country=japan')
+.then((load)=>load.json())
+    .then((data) => {
+        console.log(data);
+        for (let i = 0; i < data.length; i++){
+            let c = data[i].country;
+            let n = data[i].name;
+            let w = data[i].web_pages;
+            let table = document.querySelector('table');
+            let tableRow = document.createElement('tr');
+              tableRow.innerHTML = '<td>' + c + '</td>' + '<td>' + n + '</td>' + '<td>' + w + '</td>' ;
+            table.appendChild(tableRow);
+        }
+    })
